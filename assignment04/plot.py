2d05ee3fdb6c37b2d6c61d15dcdc421952422d87
Joseph <jlucero@sfu.ca>
2019-03-22 10:57:50 -0700

    plotter

---
+++ assignment04/plot.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+# import libraries
+import matplotlib
+import sys
+
+try:
+	import astropy.io.fits as pyfits
+except ImportError:
+	import pyfits
+
+import numpy as np
+
+from pylab import *
+from scipy.signal import medfilt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import cm
+
+def question1(data_file):
+
+	# load trajectory data
+	hdu = pyfits.open(data_file)
+
+	data = hdu[0].data
+
+	ny,nx = data.shape
+
+	# antialias using median filter
+	osy = 1; osx = 1
+
+	if (osy != 1 or osx != 1):
+		aakernel = [(osy & (~1)) + 1, (osx & (~1)) + 1]
+		data = medfilt(data, aakernel)[::osy,::osx]; ny,nx = data.shape
+		print("Antialiased with median kernel %s; output size is (%i,%i) pixels" % (aakernel,nx,ny))
+
+	# data extent and meshes
+	x0 = hdu[0].header['CRVAL1']
+	y0 = hdu[0].header['CRVAL2']
+	dx = hdu[0].header['CDELT1']
+	dy = hdu[0].header['CDELT2']
+
+	x = x0 + arange(nx)*dx*osx
+	y = y0 + arange(ny)*dy*osy
+
+	X, Y = meshgrid(x, y)
+
+	hdu.close()
+
+	abs_data_max = data.__abs__().max()
+
+	# create the figure
+	fig = figure(figsize=(10,8), frameon=False)
+
+	# plot image data
+	c = matplotlib.colors.LinearSegmentedColormap.from_list("difference", ["blue", "white", "red"])
+	im = imshow(
+		data,
+		extent=(x[0],x[-1],y[0],y[-1]), origin='lower',
+		aspect=(osx*dx)/(osy*dy), cmap=cm.get_cmap('afmhot'), interpolation='none',
+		vmin=-abs_data_max, vmax=abs_data_max # make the bounds symmetric
+		)
+
+	# plot contours
+	#contour(X, Y, data, 32, cmap=cm.jet)
+
+	# make colorbar match the plot
+	# divider = make_axes_locatable(plt.gca())
+	# cax = divider.append_axes("right", size="5%", pad=0.07)
+	cax = plt.gca()
+	fig.colorbar(im, ax=cax)
+
+	fig.savefig("question1.pdf")
+
+def question2(data_file):
+
+	# load trajectory data
+	t, energy_deviation = np.loadtxt(data_file, usecols=(0,3), unpack=True)
+
+	# create the figure
+	fig = figure(figsize=(10,8), frameon=False)
+	ax = plt.gca()
+
+	# plot image data
+	im = ax.plot(t, energy_deviation, lw=3.0)
+
+	ax.tick_params(labelsize=18)
+	ax.yaxis.offsetText.set_fontsize(18)
+	ax.yaxis.get_major_formatter().set_powerlimits((0,0))
+	ax.set_ylabel("Energy deviation", fontsize=20)
+	ax.set_xlabel("Time", fontsize=20)
+
+	fig.savefig("question2.pdf")
+
+def question3(data_file):
+
+	# load trajectory data
+	hdu = pyfits.open(data_file)
+
+	data = hdu[0].data
+
+	ny,nx = data.shape
+
+	# antialias using median filter
+	osy = 1; osx = 1
+
+	if (osy != 1 or osx != 1):
+		aakernel = [(osy & (~1)) + 1, (osx & (~1)) + 1]
+		data = medfilt(data, aakernel)[::osy,::osx]; ny,nx = data.shape
+		print("Antialiased with median kernel %s; output size is (%i,%i) pixels" % (aakernel,nx,ny))
+
+	# data extent and meshes
+	x0 = hdu[0].header['CRVAL1']
+	y0 = hdu[0].header['CRVAL2']
+	dx = hdu[0].header['CDELT1']
+	dy = hdu[0].header['CDELT2']
+
+	x = x0 + arange(nx)*dx*osx
+	y = y0 + arange(ny)*dy*osy
+
+	X, Y = meshgrid(x, y)
+
+	hdu.close()
+
+	# calculate the max of the data
+	abs_data_max = data.__abs__().max()
+
+	fig = figure(figsize=(10,8), frameon=False)
+	c = matplotlib.colors.LinearSegmentedColormap.from_list("difference", ["blue", "white", "red"])
+	im = imshow(
+		data,
+		extent=(x[0],x[-1],y[0],y[-1]), origin='lower',
+		aspect=(osx*dx)/(osy*dy), cmap=cm.get_cmap('afmhot'), interpolation='none',
+		vmin=0.0, vmax=abs_data_max
+		)
+
+	# plot contours
+	#contour(X, Y, data, 32, cmap=cm.jet)
+
+	# make colorbar match the plot
+	# divider = make_axes_locatable(plt.gca())
+	# cax = divider.append_axes("right", size="5%", pad=0.07)
+	cax = plt.gca()
+	cax.grid(True)
+	fig.colorbar(im, ax=cax)
+
+	fig.savefig("question3.pdf")
+
+if __name__ == "__main__":
+	data_file = sys.argv[1]
+	problem = sys.argv[2]
+
+	if (int(problem) == 1):
+		question1(data_file)
+	elif (int(problem) == 2):
+		question2(data_file)
+	elif (int(problem) == 3):
+		question3(data_file)
+	else:
+		"Input for problem cannot be understood."
